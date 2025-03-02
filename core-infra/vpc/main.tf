480c07e703f4575eb2bdc2176e17d71f3c3ed0e5
yashodhah <y.dominatz@gmail.com>
2025-03-02 11:51:07 +0800

    core vpc

--- core-infra/vpc/main.tf
+++ core-infra/vpc/main.tf
@@ -7,15 +7,15 @@ module "vpc" {
   version = "5.18.1"
 
   name = "reference-vpc"
-  cidr = "10.16.0.0/16" # Reference CIDR from instructor
+  cidr = "10.16.0.0/16"
 
-  azs                 = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets     = ["10.16.16.0/20", "10.16.80.0/20", "10.16.144.0/20"] # App tier
+  azs                 = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
   public_subnets      = ["10.16.48.0/20", "10.16.112.0/20", "10.16.176.0/20"] # Web tier
+  private_subnets     = ["10.16.16.0/20", "10.16.80.0/20", "10.16.144.0/20"] # App tier
   database_subnets    = ["10.16.32.0/20", "10.16.96.0/20", "10.16.160.0/20"] # DB tier
   intra_subnets       = ["10.16.0.0/20", "10.16.64.0/20", "10.16.128.0/20"] # Reserved tier
 
-  enable_nat_gateway     = true
+  enable_nat_gateway     = false
   single_nat_gateway     = true
   enable_vpn_gateway     = false
   enable_dns_support     = true
@@ -26,7 +26,6 @@ module "vpc" {
   enable_flow_log                    = true
 
   tags = {
-    Environment = "dev"
-    Project     = "core-vpc"
+    id     = "core-vpc"
   }
 }
